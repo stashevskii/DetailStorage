2b8494dc3b8648ef5c24401840cd065f147da3a2
alex <staleksandr05@gmail.com>
2025-04-18 23:00:23 +0300

    Endpoints name and description moved to settings.py

--- src/app/api/routes/detail/router.py
+++ src/app/api/routes/detail/router.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, FastAPI
 from .settings import *
-from .views import *
+from .funcs import *
 
 router = APIRouter(prefix=prefix, tags=tags)
 
-router.get("/", summary="Get detail by parameters", description="Get detail by parameters endpoint")(get_detail)
-router.post("/", summary="Add new detail", description="Add new detail endpoint")(add_detail)
-router.delete("/{id}", summary="Delete detail", description="Delete detail endpoint")(delete_detail)
-router.put("/{id}", summary="Full update detail", description="Full update detail endpoint")(full_update_detail)
-router.patch("/{id}", summary="Part update detail", description="Part update detail endpoint")(part_update_detail)
+router.get("/", summary=docs[1]["summary"], description=docs[1]["description"])(get_detail)
+router.post("/", summary=docs[2]["summary"], description=docs[2]["description"])(add_detail)
+router.delete("/{id}", summary=docs[3]["summary"], description=docs[3]["description"])(delete_detail)
+router.put("/{id}", summary=docs[4]["summary"], description=docs[4]["description"])(full_update_detail)
+router.patch("/{id}", summary=docs[5]["summary"], description=docs[5]["description"])(part_update_detail)
 
 
 def add_detail_router(app: FastAPI):
--- src/app/api/routes/detail/settings.py
+++ src/app/api/routes/detail/settings.py
@@ -1,2 +1,25 @@
 prefix = "/api/details"
 tags = ["Details"]
+
+docs = {
+    1: {
+        "summary": "Get detail by parameters",
+        "description": "Get detail by parameters endpoint"
+    },
+    2: {
+        "summary": "Add new detail",
+        "description": "Add new detail endpoint"
+    },
+    3: {
+        "summary": "Delete detail",
+        "description": "Delete detail endpoint"
+    },
+    4: {
+        "summary": "Full update detail",
+        "description": "Full update detail endpoint"
+    },
+    5: {
+        "summary": "Part update detail",
+        "description": "Part update detail endpoint"
+    }
+}
