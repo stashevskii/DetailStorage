79dd32965714efbc9f2791cf4321f1a534096ec1
alex <staleksandr05@gmail.com>
2025-05-17 20:56:26 +0300

    Fixed interfaces

--- src/app/domain/abstractions/detail.py
+++ src/app/domain/abstractions/detail.py
@@ -1,8 +1,36 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from .basic import CrudInterface, GetAllInterface
 
 
-class DetailRepositoryInterface(CrudInterface, GetAllInterface, ABC): ...
+class Repeated(ABC):
+    @abstractmethod
+    def delete(self, id, user_id):
+        raise NotImplemented
+
+    @abstractmethod
+    def replace(self, id, user_id, schema):
+        raise NotImplemented
+
+    @abstractmethod
+    def part_update(self, id, user_id, schema):
+        raise NotImplemented
 
 
-class DetailServiceInterface(CrudInterface, ABC): ...
+class DetailRepositoryInterface(GetAllInterface, Repeated, ABC):
+    @abstractmethod
+    def get(self, **kwargs):
+        raise NotImplemented
+
+    @abstractmethod
+    def add(self, **kwargs):
+        raise NotImplemented
+
+
+class DetailServiceInterface(Repeated, ABC):
+    @abstractmethod
+    def get(self, user_id, schema):
+        raise NotImplemented
+
+    @abstractmethod
+    def add(self, user_id, schema):
+        raise NotImplemented
